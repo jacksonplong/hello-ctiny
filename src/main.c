544d161ceca0ed9be43100d8b43aa44473552356
Jackson <jacksonpalmerlong@gmail.com>
2025-07-12 19:56:41 -0700

    googoo gaga

--- src/main.c
+++ src/main.c
@@ -1,15 +1,6 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define STR(x) #x
-#define XSTR(x) STR(x)
-
-#ifndef F_CPU
-#error "F_CPU is not defined! Delay functions won't work."
-#endif
-
-#pragma message XSTR(Compiling with 'F_CPU' = F_CPU)
-
 void main(void) {
 	DDRB |= (1 << PB0);
 	PORTB |= (1 << PB0);
